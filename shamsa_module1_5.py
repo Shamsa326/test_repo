ccb878117ef2bdb0c8a870192996e51acb2d775c
Shamsa326 <shamsa.shahein@gmail.com>
2025-12-17 07:11:41 +0400

    update

--- shamsa_module1_5.py
+++ shamsa_module1_5.py
@@ -1,12 +1,21 @@
 
 
-def get_today_date ():
-     
-    todays_date= datetime.datetime.now()
-    v=todays_date.strftime()
-     
-    return  v"
-     
-  
-print get_today_date ()
-    
+#python function given a string of words and price for a letter
+
+def text_price (text,price_letter):
+
+    shif_letter =""
+    price= 0
+    #count shifted letter and price 
+    for ch in text :
+        if ch.isalpha():
+            shif_letter = shif_letter + chr(ord(ch)+1 )
+            price = price + price_letter
+        else :
+            shif_letter= shif_letter + ch 
+    return { "shifted_string": shif_letter ,
+           "word_count" : len(text.split()) ,
+           "price_of_string": price
+             }
+
+print(text_price("Shamsa", 2))
