0d2feddce7c38277f5972d1b8dad566277c345a7
Shamsa326 <shamsa.shahein@gmail.com>
2025-12-23 09:10:04 +0400

    update

---
+++ module3/shamsa_module3_1.py
@@ -0,0 +1,28 @@
+
+#calling library 
+
+from fastapi import FastAPI
+
+#create fastAPI application (app it the main function that will handle all requests)
+
+app=FastAPI()
+
+#Create an endpoint (URL)
+
+@app.get("/hello")
+
+#define function and query 
+
+def hello(name: str):
+
+    return {"message": f"Hello {name}"}
+
+
+#Now run the server in the terminal:
+#>> in the terminal : uvicorn shamsa_module3_1:app --reload
+# 
+#Test your API in the browser:
+# http://127.0.0.1:8000/hello?name=Shamsa
+
+
+#End
---
+++ module3/shamsa_module3_2.py
@@ -0,0 +1,31 @@
+
+
+#define the libraries 
+
+from fastapi import FastAPI
+from datetime import datetime
+
+#create fastAPI application 
+
+app=FastAPI()
+
+
+#Create an endpoint (URL) & define function and query 
+
+@app.post("/message")
+def send_message(message: str):
+        time_now = datetime.now()
+        return {
+                "message" : message,
+                "time_stamp": time_now
+        }
+
+#Now run the server in the terminal:
+#>> in the terminal : uvicorn shamsa_module3_2:app --reload
+
+#Test API in command prompt:
+#curl -X POST "http://127.0.0.1:8000/message?message=Hello%20Hana"
+
+
+#End
+
